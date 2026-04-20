22742cb794fe187803dd6aa7b658b58e465bb657
ALFARO Victoria <ALUMNO26.ALFDARO.Victoria@ipm.edu.ar>
2026-04-20 14:55:00 -0300

    poniendole fondoo

--- scripts/jugadorMovimiento.gd
+++ scripts/jugadorMovimiento.gd
@@ -1,38 +1,29 @@
 extends CharacterBody2D
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-@onready var animationPlayer=$AnimationPlayer
-@onready var sprite=$Sprite2D
-
+const SPEED = 200.0
+const JUMP_VELOCITY = -200.0
+@onready var jugadorX=$AnimationPlayer
+@onready var textura=$Sprite2D
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("izquierda", "derecha")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-	
-	if direction==1:
-		sprite.flip.h=false
-	elif direction==-1:
-		sprite.flip.h=true
 		
-func animations (direction):
+	move_and_slide()
+	animations(direction)
+	if direction==1:
+		textura.flip_h=false
+	elif direction==-1:
+		textura.flip_h=true
+		
+func animations(direction):
 		if is_on_floor():
 			if direction==0:
-				animationPlayer.play(idle)
+				jugadorX.play("idle")
 			else:
-				animationPlayer.play(caminarR)
+				jugadorX.play("caminarR")
