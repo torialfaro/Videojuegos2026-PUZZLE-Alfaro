84a9a3870746380f82c20a2a4c59e22b9dabc571
torialfaro <ALUMNO26.ALFARO.Victoria@ipm.edu.ar>
2026-04-24 19:15:36 -0300

    fondo + tilemap de plataformas
    escalera con bugs de capas

--- scripts/jugadorMovimiento.gd
+++ scripts/jugadorMovimiento.gd
@@ -3,11 +3,16 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 @onready var jugadorX=$AnimationPlayer
 @onready var textura=$Sprite2D
+@onready var escalera=$RayCast2D
+
 func _physics_process(delta: float) -> void:
+	
+	var escaleraCollider=escalera.get_collider()
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if Input.is_action_just_pressed("saltar") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if escaleraCollider:trepar(delta)
 	var direction := Input.get_axis("izquierda", "derecha")
 	if direction:
 		velocity.x = direction * SPEED
@@ -27,3 +32,13 @@ func animations(direction):
 				jugadorX.play("idle")
 			else:
 				jugadorX.play("caminarR")
+	
+func trepar(delta):
+	var direccion=Vector2.ZERO
+	direccion.x=Input.get_axis("izquierda", "derecha")
+	direccion.y=Input.get_axis("arriba","abajo")
+	if direccion: velocity=direccion*SPEED/2
+	else: velocity=Vector2.ZERO
+	if velocity: jugadorX.play("trepar")
+	else: jugadorX.stop()
+	
