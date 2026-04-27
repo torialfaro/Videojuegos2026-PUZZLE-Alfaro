f1cc927bce9c7ef0ca26910e9d3ee1b789ad1b53
torialfaro <ALUMNO26.ALFARO.Victoria@ipm.edu.ar>
2026-04-27 17:51:36 -0300

    cooreccion escalera

--- scripts/jugadorMovimiento.gd
+++ scripts/jugadorMovimiento.gd
@@ -1,24 +1,41 @@
 extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const TREPAR_VELOCIDAD = 100.0
 @onready var jugadorX=$AnimationPlayer
 @onready var textura=$Sprite2D
-@onready var escalera=$RayCast2D
+
+var enEscalera:bool
+var trepar:bool
+
+func entraUnJugador(_body:Node2D)
 
 func _physics_process(delta: float) -> void:
-	
-	var escaleraCollider=escalera.get_collider()
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("saltar") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	if escaleraCollider:trepar(delta)
+	var enPiso=is_on_floor()
 	var direction := Input.get_axis("izquierda", "derecha")
+	if enEscalera:
+		var direccion_y=Input.get_axis("arriba","abajo")
+		if direccion_y:
+			velocity.y=direccion_y*TREPAR_VELOCIDAD
+			trepar=not enPiso
+		else:
+			velocity.y=move_toward(velocity.y,0,TREPAR_VELOCIDAD)
+			if enPiso:trepar=false
+		if trepar:
+			if direccion_y:jugadorX.play("trepar")
+			else: jugadorX.play("idle")
+	elif not enPiso:
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("saltar") and not trepar:
+		velocity.y = JUMP_VELOCITY
+		
 	if direction:
 		velocity.x = direction * SPEED
+		if enEscalera:trepar=not enPiso
+		else:trepar=false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+		if not trepar:jugadorX.play("idle")
 	move_and_slide()
 	animations(direction)
 	if direction==1:
@@ -32,13 +49,4 @@ func animations(direction):
 				jugadorX.play("idle")
 			else:
 				jugadorX.play("caminarR")
-	
-func trepar(delta):
-	var direccion=Vector2.ZERO
-	direccion.x=Input.get_axis("izquierda", "derecha")
-	direccion.y=Input.get_axis("arriba","abajo")
-	if direccion: velocity=direccion*SPEED/2
-	else: velocity=Vector2.ZERO
-	if velocity: jugadorX.play("trepar")
-	else: jugadorX.stop()
 	
