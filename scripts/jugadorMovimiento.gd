bfe5ebd3176a92969a95d172c465f6fccc779090
torialfaro <ALUMNO26.ALFARO.Victoria@ipm.edu.ar>
2026-04-29 11:31:53 -0300

    trepar escalera arreglado
    falta que le impacte la gravedad al bajar

--- scripts/jugadorMovimiento.gd
+++ scripts/jugadorMovimiento.gd
@@ -8,45 +8,43 @@ const TREPAR_VELOCIDAD = 100.0
 var enEscalera:bool
 var trepar:bool
 
-func entraUnJugador(_body:Node2D)
-
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	enEscalera=true
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	enEscalera=false
+	jugadorX.play("idle")
+	
 func _physics_process(delta: float) -> void:
 	var enPiso=is_on_floor()
 	var direction := Input.get_axis("izquierda", "derecha")
 	if enEscalera:
-		var direccion_y=Input.get_axis("arriba","abajo")
-		if direccion_y:
+		var direccion_y=Input.get_axis("saltar","abajo")
+		if direccion_y!=0:
 			velocity.y=direccion_y*TREPAR_VELOCIDAD
 			trepar=not enPiso
 		else:
 			velocity.y=move_toward(velocity.y,0,TREPAR_VELOCIDAD)
 			if enPiso:trepar=false
 		if trepar:
-			if direccion_y:jugadorX.play("trepar")
+			if direccion_y!=0:jugadorX.play("trepar")
 			else: jugadorX.play("idle")
 	elif not enPiso:
 		velocity += get_gravity() * delta
 	if Input.is_action_just_pressed("saltar") and not trepar:
 		velocity.y = JUMP_VELOCITY
 		
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED
-		if enEscalera:trepar=not enPiso
-		else:trepar=false
+		textura.flip_h = direction < 0
+		if enEscalera:
+			trepar = not enPiso
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if not trepar:jugadorX.play("idle")
 	move_and_slide()
-	animations(direction)
-	if direction==1:
-		textura.flip_h=false
-	elif direction==-1:
-		textura.flip_h=true
-		
+	if not trepar:animations(direction)
 func animations(direction):
 		if is_on_floor():
 			if direction==0:
 				jugadorX.play("idle")
 			else:
 				jugadorX.play("caminarR")
-	
